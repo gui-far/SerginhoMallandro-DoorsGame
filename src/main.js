80899fb65f5bd3bbf603cfbefe8225f88837c8af
Gui Far <gui.far@outlook.com>
2020-06-03 00:24:37 -0300

    Working!

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+    el: '#app',  
+    render(createElement) {
+        return createElement(App)
+    }
+})
+
+
+/*
+ou
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
+
+*/
+
